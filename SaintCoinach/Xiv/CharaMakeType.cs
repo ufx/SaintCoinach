341c796d708b035f4807f3893f8aa31bcdf06c74
Johto <darkninjanaut@gmail.com>
2019-04-04 03:06:25 -0400

    Added CharaMakeType.cs

---
+++ SaintCoinach/Xiv/CharaMakeType.cs
@@ -0,0 +1,56 @@
+﻿using SaintCoinach.Ex.Relational;
+using SaintCoinach.Imaging;
+using System.Collections.Generic;
+
+namespace SaintCoinach.Xiv {
+    public class CharaMakeType : XivRow, IXivRow {
+        private CharaMakeFeatureIcon[] _Items;
+        #region Properties
+        public Race Race { get { return As<Race>(); } }
+        public Tribe Tribe { get { return As<Tribe>(); } }
+        public IEnumerable<CharaMakeFeatureIcon> FacialFeatureIcon { get { return _Items ?? (_Items = BuildItems()); } }
+        public int Gender { get { return AsInt32("Gender"); } }
+        #endregion
+
+        #region Constructors
+
+        public CharaMakeType(IXivSheet sheet, IRelationalRow sourceRow) : base(sheet, sourceRow) { }
+        #region Build
+
+        private CharaMakeFeatureIcon[] BuildItems() {
+            const int Count = 42;
+
+            var items = new List<CharaMakeFeatureIcon>();
+
+            for (var i = 0; i < Count; ++i) {
+                var item = AsImage("FacialFeatureIcon", i);
+                if (item == null) continue;
+                items.Add(new CharaMakeFeatureIcon(item, i));
+            }
+
+            return items.ToArray();
+        }
+        public class CharaMakeFeatureIcon {
+            #region Properties
+
+            public ImageFile FacialFeatureIcon { get; private set; }
+            public int Count { get; private set; }
+            #endregion
+
+            #region Constructors
+
+            #region Constructor
+
+            public CharaMakeFeatureIcon(ImageFile image, int count) {
+                FacialFeatureIcon = image;
+                Count = count;
+            }
+
+            #endregion
+
+            #endregion
+        }
+        #endregion
+        #endregion
+    }
+}
--- SaintCoinach/Xiv/Item.cs
+++ SaintCoinach/Xiv/Item.cs
@@ -119,7 +119,11 @@ namespace SaintCoinach.Xiv {
         /// </summary>
         /// <value>The price in Gil of the current item in shops.</value>
         public int Ask { get { return AsInt32("Price{Mid}"); } }
-        public int EquipSlotCategory { get { return AsInt32("EquipSlotCategory"); } }
+        /// <summary>
+        ///     Gets the <see cref="EquipSlotCategory" /> of the current item.
+        /// </summary>
+        /// <value>The <see cref="EquipSlotCategory" /> of the current item.</value>
+        public EquipSlotCategory EquipSlotCategory { get { return As<EquipSlotCategory>(); } }
 
         /// <summary>
         ///     Gets the recipes using the current item as material.
